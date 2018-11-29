27e7051c743ca53f9c76d7ee15c106e86b71c8e6
yoraish <yorai.shaoul@gmail.com>
2018-11-29 11:36:00 -0500

    adding dijkstra to generate a coord list from current position to set goal in the map frame

--- mover/src/mover.py
+++ mover/src/mover.py
@@ -3,6 +3,8 @@ import rospy
 from std_msgs.msg import String
 from nav_msgs.msg import *
 from tf import *
+from collections import defaultdict
+
 
 class Mover():
     def __init__(self):
@@ -21,7 +23,7 @@ class Mover():
         rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
 
         # subscribe to /odom topic to get the position of the robot with respect to the odom origin
-        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        rospy.Subscriber('/odom', Odometry, self.odom_callback, queue_size =10)
         # spin() simply keeps python from exiting until this node is stopped
         rospy.spin()
 
@@ -37,10 +39,6 @@ class Mover():
         self.grid_res = map_meta_data.resolution
         self.grid_w = map_meta_data.width
         self.grid_h = map_meta_data.height
-
-
-
-
 
         rospy.loginfo('Got new map with res %s', self.grid_res)
 
@@ -58,9 +56,21 @@ class Mover():
         pixel_x, pixel_y = self.map_coord_to_pixel(self.map_x, self.map_y)
 
         # get the value of the occupancy grid in that pixel
-        pixe_value = self.grid_pixel_value(pixel_x, pixel_y)
-        self.show_surrounding_pixels()
-        # rospy.loginfo('transform to map frame %s', self.odom_to_map_transformer(self.odom_x, self.odom_y))
+        pixel_value = self.grid_pixel_value(pixel_x, pixel_y)
+        # self.show_surrounding_pixels()
+
+
+        rospy.loginfo('transform to map frame %s', self.odom_to_map_transformer(self.odom_x, self.odom_y))
+        # rospy.loginfo('transform to map again %s', self.pixel_to_map_coord(pixel_x, pixel_y))
+
+        print('WE HERE')
+        goal_pixel = self.map_coord_to_pixel(0.5,0.5)
+        goal_pixel_x = goal_pixel[0]
+        goal_pixel_y = goal_pixel[1]
+
+        start_pixel = (pixel_x,pixel_y)
+
+        print(self.dijkstra_to_goal(self.weighted_graph(), start_pixel, goal_pixel))
 
 
     def odom_to_map_transformer(self, init_x, init_y):
@@ -82,6 +92,14 @@ class Mover():
         pixel_y = round((y - self.grid_origin_y)/self.grid_res)
         return (pixel_x, pixel_y)
 
+    def pixel_to_map_coord(self, pixel_x, pixel_y):
+        '''
+        converts occupancy grid pixel value to coord in map frame
+        '''
+        map_x = pixel_x*self.grid_res + self.grid_origin_x
+        map_y = pixel_y*self.grid_res + self.grid_origin_y
+        return (map_x, map_y)
+
     def grid_pixel_value(self, pixel_x, pixel_y):
         '''
         return the occupancy value [0,100] of the pixel of interest in the occupancy grid
@@ -99,7 +117,113 @@ class Mover():
             x, y = self.map_coord_to_pixel(self.map_x + x_delta, self.map_y)
             surrounding_list.append(self.grid_pixel_value(x,y))
         print(surrounding_list)
-            
+
+    def get_possible_coords(self, x, y, grid = None):
+
+        if grid == None:
+            grid = self.grid_data
+
+        # the allowed moves are up down left right
+        # return a list of tuples that only include coordinates of cells that are empty
+        tries = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
+        finals = []
+        for coord in tries:
+            if coord[0] < self.grid_w and coord[0] >= 0 and coord[1] < self.grid_h and coord[1] >= 0:
+                if self.get_item(coord[0], coord[1], grid) in [0,'G']:
+                    finals.append(coord)
+        return finals 
+
+    def weighted_graph(self):
+        weighted_graph = {}
+        for x in range(self.grid_w):
+            for y in range (self.grid_h):
+                coord = (x,y)
+                # get surrounding coords
+                neighbors = self.get_possible_coords(x,y,self.grid_data)
+                # add a dict {node:{neighbor1:dist1, neighbor2: dist2}} to weighted graph. In the simple grid, all distances will be 1
+                neighbors_dict = {}
+                for neighbor in neighbors:
+                    neighbors_dict[neighbor] = 1
+                # add the node with its neighbors to the weighted graph
+                weighted_graph[coord] = neighbors_dict
+        return weighted_graph
+
+    def x_y_to_index(self, x, y):
+        return x + y*self.grid_w
+
+    def get_item(self, x, y, grid = None):
+        if grid == None:
+            grid = self.grid_data
+        return grid[self.x_y_to_index(x,y)]
+    
+    def dijkstra_to_goal(self, weighted_graph, start, end):
+        '''
+        generates a list of (x,y) coords in the grid frame from current position to specified goal
+        which is also (x,y) in grid frame
+        '''
+        """
+        Calculate the shortest path for a directed weighted graph.
+
+        Node can be virtually any hashable datatype.
+
+        :param start: starting node
+        :param end: ending node
+        :param weighted_graph: {"node1": {"node2": "weight", ...}, ...}
+        :return: ["START", ... nodes between ..., "END"] or None, if there is no
+                path
+        """
+
+        # We always need to visit the start
+        nodes_to_visit = {start}
+        visited_nodes = set()
+        distance_from_start = defaultdict(lambda: float("inf"))
+        # Distance from start to start is 0
+        distance_from_start[start] = 0
+        tentative_parents = {}
+
+        while nodes_to_visit:
+            # The next node should be the one with the smallest weight
+            current = min(
+                [(distance_from_start[node], node) for node in nodes_to_visit])[1]
+
+            # The end was reached
+            if current == end:
+                break
+
+            nodes_to_visit.discard(current)
+            visited_nodes.add(current)
+
+            # print('weighted_graph[current]', weighted_graph[current])
+
+            for neighbour, distance in weighted_graph[current].items():
+                if neighbour in visited_nodes:
+                    continue
+                neighbour_distance = distance_from_start[current] + distance
+                if neighbour_distance < distance_from_start[neighbour]:
+                    distance_from_start[neighbour] = neighbour_distance
+                    tentative_parents[neighbour] = current
+                    nodes_to_visit.add(neighbour)
+
+        return self._deconstruct_path(tentative_parents, end)
+
+
+    def _deconstruct_path(self, tentative_parents, end):
+        if end not in tentative_parents:
+            return None
+        cursor = end
+        path = []
+        while cursor:
+            path.append(cursor)
+            cursor = tentative_parents.get(cursor)
+
+        # convert to map frame
+
+        pixel_list = list(reversed(path))
+        map_coord_list= []
+        for pixel_x, pixel_y in pixel_list:
+            map_coord_list.append((self.pixel_to_map_coord(pixel_x, pixel_y)))
+        return map_coord_list
+                
 
 
 
@@ -117,7 +241,8 @@ def main():
     rospy.init_node('mover', anonymous=True)
 
     # create new instance of the class
-    Mover()
+    mover = Mover()
+
 
     
 
@@ -125,4 +250,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    
